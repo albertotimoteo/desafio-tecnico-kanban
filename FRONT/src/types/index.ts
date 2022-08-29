755ebe34329f80b0e3f7f0fb057c24438c1bf53d
Alberto Timoteo <alberto.pptimoteo@gmail.com>
2022-08-29 01:21:41 -0300

    feat: created types file with all types used in the project

--- FRONT/src/types/index.ts
+++ FRONT/src/types/index.ts
@@ -1,6 +1,44 @@
-export type Card = {
+export type CardProperties = {
   id: string
   titulo: string
   conteudo: string
   lista: string
+}
+
+export type Icons =
+  | 'ADD'
+  | 'DELETE'
+  | 'RIGHT'
+  | 'LEFT'
+  | 'EDIT'
+  | 'CANCEL'
+  | 'SAVE'
+
+export type CardMode = 'EDIT' | 'VIEW'
+
+export type Lists = 'New' | 'ToDo' | 'Doing' | 'Done'
+
+export type AddCardFunction = (title: string, content: string) => void
+
+export type AlterCardFunction = (
+  title: string,
+  content: string,
+  list: Lists,
+  id: string
+) => void
+
+export type DeleteCardFunction = (id: string) => void
+
+export type GetIconsObject = {
+  [key in Icons]: JSX.Element
+}
+
+export type GetTitlesOjbect = {
+  [key in Lists]: string
+}
+
+export type Actions = {
+  add: AddCardFunction
+  alter: AlterCardFunction
+  delete: DeleteCardFunction
 }
