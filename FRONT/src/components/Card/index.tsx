1a2d098f6af06628ffd68a980a8b98849d27dd8f
Alberto Timoteo <alberto.pptimoteo@gmail.com>
2022-08-29 01:19:16 -0300

    feat: created card component

---
+++ FRONT/src/components/Card/index.tsx
@@ -0,0 +1,96 @@
+import { useState } from 'react'
+import {
+  AddCardFunction,
+  AlterCardFunction,
+  CardMode,
+  DeleteCardFunction,
+  Lists,
+} from '../../types'
+import { getNextList, getPreviousList } from '../../utils/lists'
+import { CardFooter } from '../CardFooter'
+import { IconButton } from '../IconButton'
+import {
+  Container,
+  Content,
+  ContentTextArea,
+  HeaderFooter,
+  Title,
+  TitleInput,
+} from './style'
+
+type CardProps = {
+  title: string
+  content: string
+  list: Lists
+  id?: string
+  alterCard?: AlterCardFunction
+  deleteCard?: DeleteCardFunction
+  addCard?: AddCardFunction
+  isNew?: boolean
+}
+
+export const Card = ({
+  title,
+  content,
+  list,
+  id,
+  alterCard,
+  deleteCard,
+  addCard,
+  isNew,
+}: CardProps) => {
+  const [mode, setMode] = useState<CardMode>('VIEW')
+  const [titleEdit, setTitleEdit] = useState(title)
+  const [contentEdit, setContentEdit] = useState(content)
+
+  return (
+    <Container>
+      <HeaderFooter>
+        {mode === 'EDIT' || isNew ? (
+          <TitleInput
+            value={titleEdit}
+            onChange={event => setTitleEdit(event.target.value)}
+          />
+        ) : (
+          <>
+            <Title>{title}</Title>
+            <IconButton icon="EDIT" onClick={() => setMode('EDIT')} />
+          </>
+        )}
+      </HeaderFooter>
+      {mode === 'EDIT' || isNew ? (
+        <ContentTextArea
+          value={contentEdit}
+          onChange={event => setContentEdit(event.target.value)}
+        />
+      ) : (
+        <Content>{content}</Content>
+      )}
+      <HeaderFooter>
+        <CardFooter
+          isNew={Boolean(isNew)}
+          mode={mode}
+          list={list}
+          onClickRight={() =>
+            alterCard && alterCard(title, content, getNextList(list), id ?? '')
+          }
+          onClickLeft={() =>
+            alterCard &&
+            alterCard(title, content, getPreviousList(list), id ?? '')
+          }
+          onClickDelete={() => deleteCard && deleteCard(id ?? '')}
+          onClickAdd={() => {
+            addCard && addCard(titleEdit, contentEdit)
+            setTitleEdit('')
+            setContentEdit('')
+          }}
+          onClickSave={() => {
+            alterCard && alterCard(titleEdit, contentEdit, list, id ?? '')
+            setMode('VIEW')
+          }}
+          onClickCancel={() => setMode('VIEW')}
+        />
+      </HeaderFooter>
+    </Container>
+  )
+}
---
+++ FRONT/src/components/Card/style.ts
@@ -0,0 +1,53 @@
+import styled from 'styled-components'
+import { colors, fontSizes } from '../../theme'
+
+export const Container = styled.div`
+  border-radius: 25px;
+  background: ${colors.gray1};
+  min-width: 200px;
+  max-height: 160px;
+  box-shadow: 2px 4px 3px rgba(0, 0, 0, 0.28);
+  padding: 15px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  gap: 6px;
+`
+
+export const HeaderFooter = styled.header`
+  display: flex;
+  justify-content: space-between;
+`
+
+export const Title = styled.h3`
+  margin: 0;
+  font-size: ${fontSizes.headlineMedium};
+`
+
+export const Content = styled.section`
+  font-size: ${fontSizes.body};
+  overflow-y: auto;
+`
+
+export const TitleInput = styled.input`
+  border-radius: 10px;
+  border: ${`0.5px ${colors.typography} solid`};
+  width: 100%;
+  padding: 6px;
+  &:focus {
+    outline: none;
+    border: ${`1.5px ${colors.typography} solid`};
+  }
+`
+
+export const ContentTextArea = styled.textarea`
+  border: ${`0.5px ${colors.typography} solid`};
+  width: 93%;
+  min-height: 60px;
+  border-radius: 10px;
+  padding: 6px;
+  &:focus {
+    outline: none;
+    border: ${`1.5px ${colors.typography} solid`};
+  }
+`
