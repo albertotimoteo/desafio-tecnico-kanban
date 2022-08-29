5f7199b3c884431e6fe682892571dd1693f9788a
Alberto Timoteo <alberto.pptimoteo@gmail.com>
2022-08-29 01:22:40 -0300

    feat: added kanban page to app and added loading logic

---
+++ FRONT/src/App.style.ts
@@ -0,0 +1,7 @@
+import styled from 'styled-components'
+import { colors, fontFamily } from './theme'
+
+export const AppWrapper = styled.div`
+  font-family: ${fontFamily};
+  color: ${colors.typography};
+`
--- FRONT/src/App.tsx
+++ FRONT/src/App.tsx
@@ -1,22 +1,27 @@
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import { login } from './services/login'
 import api from './services/configuration'
+import { AppWrapper } from './App.style'
+import { Kanban } from './pages/Kanban'
+import { Loading } from './components/Loading'
 
 function App() {
+  const [isLoading, setLoading] = useState(true)
+
   useEffect(() => {
     const doLogin = async () => {
       try {
         const response = await login()
         api.defaults.headers.common.Authorization = `Bearer ${response.data}`
+        setLoading(false)
       } catch (error) {
         console.log(error)
       }
     }
-
     doLogin()
   }, [])
 
-  return <div></div>
+  return <AppWrapper>{isLoading ? <Loading /> : <Kanban />}</AppWrapper>
 }
 
 export default App
