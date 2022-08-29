e0cfef65fc88d5b83b37899ca3e18b83986c165a
Alberto Timoteo <alberto.pptimoteo@gmail.com>
2022-08-29 01:20:55 -0300

    feat: created project theme

---
+++ FRONT/src/theme/index.ts
@@ -0,0 +1,19 @@
+export const colors = {
+  primary: '#3A72F8',
+  typography: '#575757',
+  gray1: '#F8F8F8',
+  gray2: '#EAEAEA',
+}
+
+export const iconProps = {
+  color: colors.primary,
+  size: 18,
+}
+
+export const fontSizes = {
+  headlineBig: '26px',
+  headlineMedium: '18px',
+  body: '14px',
+}
+
+export const fontFamily = 'Poppins'
