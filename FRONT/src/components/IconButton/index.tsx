bd09ca233ebee231269ab186190b8d5786c25c06
Alberto Timoteo <alberto.pptimoteo@gmail.com>
2022-08-29 01:19:05 -0300

    feat: created icon button component

---
+++ FRONT/src/components/IconButton/index.tsx
@@ -0,0 +1,12 @@
+import { Icons } from '../../types'
+import { icons } from '../../utils/icons'
+import { Button } from './style'
+
+type IconButtonProps = {
+  icon: Icons
+  onClick?: () => void
+}
+
+export const IconButton = ({ icon, onClick }: IconButtonProps) => (
+  <Button onClick={onClick}>{icons[icon]}</Button>
+)
