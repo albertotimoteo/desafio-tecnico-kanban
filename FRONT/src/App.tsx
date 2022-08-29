243649cfa5b653489791597f064e088df71f2684
Alberto Timoteo <alberto.pptimoteo@gmail.com>
2022-08-28 21:05:39 -0300

    feat: created first token retrieval

--- FRONT/src/App.tsx
+++ FRONT/src/App.tsx
@@ -1,26 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useEffect } from 'react'
+import { login } from './services/login'
+import api from './services/configuration'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  useEffect(() => {
+    const doLogin = async () => {
+      try {
+        const response = await login()
+        api.defaults.headers.common.Authorization = `Bearer ${response.data}`
+      } catch (error) {
+        console.log(error)
+      }
+    }
+
+    doLogin()
+  }, [])
+
+  return <div></div>
 }
 
-export default App;
+export default App
